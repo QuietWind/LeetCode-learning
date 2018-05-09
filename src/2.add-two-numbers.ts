15ff7dd3784e523b3ebae9becc5d4d4e79154979
陈欢 <jingfeng_quietwind@163.com>
2018-05-09 14:15:07 +0800

    完成第二题

---
+++ src/2.add-two-numbers.ts
@@ -0,0 +1,67 @@
+export class ListNode {
+  public next: ListNode | null;
+  constructor(public val: number) {
+    this.next = null;
+  }
+}
+
+export function parseNumArrToNode(numArr: number[]): ListNode {
+  let index = 0;
+  let rn = new ListNode(0);
+  let prev: ListNode | null = null;
+
+  while (numArr[index] !== undefined) {
+    const node = new ListNode(numArr[index]);
+    if (prev) {
+      prev.next = node;
+    } else {
+      rn = node;
+    }
+    prev = node;
+    index++;
+  }
+
+  return rn;
+}
+
+export const parseNodeToNumArr = (
+  node: ListNode,
+  prefixNumArr: number[] = []
+): number[] => {
+  const currentNum = node.val;
+  const nodeStr: number[] = prefixNumArr.concat(currentNum);
+
+  if (node.next !== null) {
+    return parseNodeToNumArr(node.next, nodeStr);
+  }
+
+  return nodeStr;
+};
+
+export function addTwoNumbers(l1: ListNode, l2: ListNode): ListNode {
+  const rNode = new ListNode(0);
+  let prev: ListNode | null = l1;
+  let next: ListNode | null = l2;
+  let currentNode = rNode;
+  let carry = 0;
+
+  while (prev !== null || next !== null) {
+    const sum = (prev ? prev.val : 0) + (next ? next.val : 0) + carry;
+    const val = sum >= 10 ? sum % 10 : sum;
+    const node = new ListNode(val);
+
+    carry = Math.floor(sum / 10);
+
+    currentNode.next = node;
+    currentNode = node;
+
+    prev = prev ? prev.next : null;
+    next = next ? next.next : null;
+  }
+
+  if (carry > 0) {
+    currentNode.next = new ListNode(carry);
+  }
+
+  return rNode.next ? rNode.next : new ListNode(0);
+}
